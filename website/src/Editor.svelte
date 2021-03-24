1c01f0453aa63727f5965708b39f3f10b7a5975d
Virgiel <>
2021-03-24 19:23:08 +0100

    Remove garbage code editor code with simpler logic based on editable content

--- website/src/Editor.svelte
+++ website/src/Editor.svelte
@@ -1,34 +1,143 @@
 <script>
-  import EditorView from './EditorView.svelte';
-  import { newMultilineEditor } from './multineEditor';
+  import { onMount } from 'svelte';
+  import { CodeJar } from 'codejar';
   import { load } from './wasm';
 
-  let width;
+  let editor;
+  let result = 'Loading...';
 
-  async function editor() {
-    await load();
-    return newMultilineEditor();
-  }
+  const defaultCode = `
+# Welcome into the Tiny Mathematic Language online editor
+
+# You can perform simple operations
+1+1
+1+2+3+4+5+6+7+8+9
+3*3
+(3/8) * (32+4) + 34
+2+2
+8/(2*(2+2))
+
+# Use constants
+PI
+cos(PI)
+cos(PI/2)
+sin(23*PI)
+
+# Use mathematical function
+cos(5)
+sin(4/8)
+log2(1000)
+exp(ln(7))
+10 / 3
+trunc(10/3)
+fract(10/3)
+
+# And print text
+
+"J'aime le chocolat"
+"Full: "1/3"   Truncated: "trunc(1/3)"    Decimal: "fract(1/3)
+
+# And now somethings more interesting
+a = 12
+b = 32
+"a = "a" & b = "b
+"hypotenuse = sqrt(a*a+b*b)"
+"hypotenuse = "sqrt(a*a+b*b)
+
+`;
+  let lineHeight = [];
+
+  onMount(() => {
+    load().then(wasm => {
+      let isInit = false;
+      const jar = CodeJar(editor, editor => {
+        if (!isInit) {
+          editor.style.whiteSpace = 'pre';
+          editor.style.resize = 'none';
+        }
+        editor.innerHTML = wasm
+          .highlight_batch(editor.textContent)
+          .map((line, idx) => {
+            let height = lineHeight[idx] | 1;
+            if (height > 1) {
+              return (
+                line +
+                `<span class="spacer" style="line-height:${
+                  height * 20
+                }px;">\n</span>`
+              );
+            } else {
+              return line + '\n';
+            }
+          })
+          .join('');
+      });
+      const onUpdate = code => {
+        result = wasm
+          .execute_batch(code)
+          .map((line, idx) => {
+            lineHeight[idx] = line.split('\n').length;
+            return line;
+          })
+          .join('\n');
+        console.log(lineHeight);
+      };
+      jar.onUpdate(onUpdate);
+      onUpdate(defaultCode);
+    });
+  });
 </script>
 
-<div class="screen" bind:offsetWidth={width}>
-  {#await editor()}
-    <p>Loading ...</p>
-  {:then editor}
-    <EditorView {editor} sidePanel={width > 700} />
-  {/await}
+<div class="screen">
+  <div class="editor" bind:this={editor} resize="none">
+    {defaultCode}
+  </div>
+  <div class="result">
+    {@html result}
+  </div>
 </div>
 
 <style>
   .screen {
-    display: grid;
+    display: flex;
     height: 100vh;
     width: 100vw;
+    flex-flow: row;
   }
-  p {
-    margin: auto;
-    font-size: 1.5em;
-    font-weight: bold;
-    text-align: center;
+  .screen * {
+    height: 100vh;
+    width: 50vw;
+  }
+
+  @media (max-width: 800px) {
+    .screen {
+      flex-flow: column;
+    }
+    .screen * {
+      height: 50vh;
+      width: 100vw;
+    }
+  }
+  .screen * {
+    overflow: auto;
+    line-height: 20px;
+    padding: 10px;
+    tab-size: 4;
+    white-space: pre;
+  }
+  .result {
+    background-color: #333333;
+  }
+  div::-webkit-scrollbar-track {
+    background: none;
+  }
+  div::-webkit-scrollbar {
+    width: 12px;
+  }
+  div::-webkit-scrollbar-corner {
+    background-color: #92837490;
+  }
+  div::-webkit-scrollbar-thumb {
+    background-color: #92837435;
   }
 </style>
